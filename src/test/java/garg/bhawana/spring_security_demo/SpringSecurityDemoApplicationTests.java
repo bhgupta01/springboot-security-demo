4110986b7522354f211611f4913de49f4ae884cd
Bhawana Gupta <12906412+bhgupta01@users.noreply.github.com>
2025-05-29 11:14:48 +0530

    Initial commit

---
+++ src/test/java/garg/bhawana/spring_security_demo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package garg.bhawana.spring_security_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
